03f88c0725f58e71c83338657223b73277149272
IvanDePaola <ivan.depaola@students.fhnw.ch>
2022-05-23 13:08:07 +0200

    User Klasse angepasst, Variablen erstellt und Methoden

--- Team7_Claim/src/main/java/ch/model/User.java
+++ Team7_Claim/src/main/java/ch/model/User.java
@@ -1,14 +1,31 @@
 package ch.model;
 
+import java.util.Random;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+//Autor: Ivan De Paola
 @Entity
 public class User {
 	
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
 	private Long id;
 	private String userName;
 	private String email;
 	private String password;
-	private UserRolle userRolle;
+	private String userRolle;
+	private boolean verifaction;
+	private int winner;
+	private int loser;
+	private int gamesPlayed;
+	private int score;
 	
+	
+	//Getters and setters
 	
 	public Long getId() {
 		return id;
@@ -34,12 +51,81 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public UserRolle getUserRolle() {
+	public String getUserRolle() {
 		return userRolle;
 	}
-	public void setUserRolle(UserRolle userRolle) {
+	public void setUserRolle(String userRolle) {
 		this.userRolle = userRolle;
 	}
+	public boolean isVerifaction() {
+		return verifaction;
+	}
+	public void setVerifaction(boolean verifaction) {
+		this.verifaction = verifaction;
+	}
+	public int getWinner() {
+		return winner;
+	}
+	public void setWinner(int winner) {
+		this.winner = winner;
+	}
+	public int getLoser() {
+		return loser;
+	}
+	public void setLoser(int loser) {
+		this.loser = loser;
+	}
+	public int getGamesPlayed() {
+		return gamesPlayed;
+	}
+	public void setGamesPlayed(int gamesPlayed) {
+		this.gamesPlayed = gamesPlayed;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	// methods
+	
+	public void increaseWinner() {
+		this.winner++;
+		this.score += 40;
+	}
+	
+	public void increaseLoser() {
+		this.loser++;
+		this.score += 10;
+	}
+	
+	public void increaseGamesPlayed() {
+		this.gamesPlayed++;
+	}
+	
+	public int newToken() {
+		Random rdm = new Random();
+		Integer secr = rdm.nextInt(999999) + 100000;
+		return secr;
+	}	
+	
+	// constructor
+	public User(String userName, String email, String password, String userRolle, boolean verifaction,
+			int winner, int loser, int gamesPlayed, int score) {
+		super();
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+		this.userRolle = userRolle;
+		this.verifaction = false;
+		this.winner = 0;
+		this.loser = 0;
+		this.gamesPlayed = 0;
+		this.score = 0;
+	}
+	
+	
 	
 	
 	
