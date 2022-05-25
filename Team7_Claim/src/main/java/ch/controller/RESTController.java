720001fc20f56cae3dcd0bad8163f6819afe03b4
robinheiz <robinheiz@air-von-robin.edu.ds.fhnw.ch>
2022-05-25 14:24:31 +0200

    restcontroller klasse gestartet, javamailsender dependency geaddet

--- Team7_Claim/src/main/java/ch/controller/RESTController.java
+++ Team7_Claim/src/main/java/ch/controller/RESTController.java
@@ -1,10 +1,18 @@
 package ch.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
 public class RESTController {
+	@Autowired
+	private JavaMailSender mailSender;
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	public RegisterUser
 
 }
