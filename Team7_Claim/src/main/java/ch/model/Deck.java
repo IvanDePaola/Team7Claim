b95c022b1c9036f61ad2750bc68b203b3283a5b6
robinheiz <robinheiz@air-von-robin.edu.ds.fhnw.ch>
2022-05-30 13:14:24 +0200

    deck klasse mit misch methode ergänzt

--- Team7_Claim/src/main/java/ch/model/Deck.java
+++ Team7_Claim/src/main/java/ch/model/Deck.java
@@ -1,18 +1,70 @@
 package ch.model;
 
 import java.util.ArrayList;
-
+import java.util.Collections;
+// Autor Robin Heiz
 public class Deck {
 	
-	private final int MAX_CARDS = 10;
+	private ArrayList<Card> deckOfCards;
+	private final int MAXCARDS = 10;
 	
-	private ArrayList<Card> deck;
+	
+	
 	
 	public Deck() {
-		deck = new ArrayList<Card>();
+		deckOfCards = new ArrayList<Card>();
+		// Zuweisung Race / Deck Mischen
+		for (int z = 0; z < 5; z++) {
+			switch (z) {
+			
+			case 1:
+				for (int i = 0; i < MAXCARDS; i++) {
+					Card c = new Card(Race.Undead, i);
+					deckOfCards.add(c);
+				}break;
+			case 2:
+				for (int i = 0; i < MAXCARDS; i++) {
+					Card c = new Card(Race.Doppelgaenger, i);
+					deckOfCards.add(c);
+				}break;
+			case 3:
+				for (int i = 0; i < MAXCARDS; i++) {
+					Card c = new Card(Race.Dwarfs, i);
+					deckOfCards.add(c);
+				}break;
+			case 4:
+				for (int i = 2; i < MAXCARDS; i++) {
+					Card c = new Card(Race.Knights, i);
+					deckOfCards.add(c);
+				}break;
+				
+			case 5:
+				for (int i = 0; i < MAXCARDS; i++) {
+					Card c = new Card(Race.Goblin, i);
+					deckOfCards.add(c);
+					if(i == 0) {
+						for (int t = 0; t < 4; t++) {
+							Card k = new Card(Race.Goblin, 0);
+							deckOfCards.add(k);
+						}
+					}
+				}
+				Collections.shuffle(deckOfCards);
+			
+			
+			}
+			
+		}
 		
-		ArrayList List = new ArrayList<>();
+	}
+
+
+
+	private ArrayList<Card> getDeck() {
+		return deckOfCards;
 		
 	}
 	
 }
+	
+
